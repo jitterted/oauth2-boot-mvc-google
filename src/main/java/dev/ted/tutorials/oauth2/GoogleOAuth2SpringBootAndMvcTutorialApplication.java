5fa58ec5c476ea01b6ba1c6aa780efe8d7ff725a
Ted M. Young <tedyoung@gmail.com>
2019-03-13 13:10:40 -0700

    Initial project creation from Spring Initializer.

---
+++ src/main/java/dev/ted/tutorials/oauth2/GoogleOAuth2SpringBootAndMvcTutorialApplication.java
@@ -0,0 +1,13 @@
+package dev.ted.tutorials.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleOAuth2SpringBootAndMvcTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleOAuth2SpringBootAndMvcTutorialApplication.class, args);
+	}
+
+}
